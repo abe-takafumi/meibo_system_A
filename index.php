93549a5b3dd3d210d675d275d299e9f0d7c1f145
teruiyuki <terui-yuki@webrage.jp>
2021-08-23 11:27:49 +0900

    [update]"検索結果なし"の際の表示に関して。if文によるエラー文の表示をなくしたこと
    検索結果なしの表示方法を表の中に変更した。if文によるエラーがなくしたこと。

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
     </head>
     <body>
         <?php
+            //DBのデータを参照する
             $DB_DSN = "mysql:host=localhost; dbname=tabe; charset=utf8";
             $DB_USER = "webaccess";
             $DB_PW = "toMeu4rH";
             $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
 
-            require './include/common.php';
+            //共有フォルダを参照する
+            require './include/common_no0.php';
         ?>
 
         <h1>社員名簿システム</h1>
@@ -25,25 +27,16 @@
             名前：<input type="text" name="name" size="30" maxlength="30">
 
             性別：<select name="seibetu">
-                <option value="0" selected="selected">すべて</option>
+                <option value="" selected="selected">すべて</option>
                 <option value="1">男</option>
                 <option value="2">女</option>
             </select>
 
-            年齢：<input type="text" name="age"><br>
-
-            都道府県：<select name="pref">
-            <?php
-                $name='name="pref"';
-                foreach ($pref as $key => $value){
-                    echo "<option ". $name ."value=". $key .">" . $value . "</option>";
-                }
-            ?></select>
-
             部署：<select name="section_ID">
             <?php
                 $name='name="section_ID"';
-                foreach ($section_ID as $key => $value){
+                echo '<option value="" selected="selected">すべて</option>';
+                foreach ($section_ID_array as $key => $value){
                     echo "<option ". $name ."value=". $key .">" . $value . "</option>";
                 }
             ?></select>
@@ -51,7 +44,8 @@
             役職：<select name="grade_ID">
             <?php
                 $name='name="section_ID"';
-                foreach ($grade_ID as $key => $value){
+                echo '<option value="" selected="selected">すべて</option>';
+                foreach ($grade_ID_array as $key => $value){
                     echo "<option ". $name ."value=". $key .">" . $value . "</option>";
                 }
             ?></select><br>
@@ -63,6 +57,15 @@
 
         <hr color="#00ff00" size="3">
 
+        <table border="1" style="border-collapse:collapse" align="center">
+            <tr>
+                <th>社員ID</th>
+                <th>名前</th>
+                <th>性別</th>
+                <th>部署</th>
+                <th>役職</th>
+            </tr>
+
             <?php
                 $where_str = "";
                 $query_str = "SELECT * FROM member WHERE 1 ";
@@ -71,23 +74,15 @@
                     $where_str .= "AND name LIKE '%" . $_GET['name'] . "%' ";
                 }
 
-                if($_GET['seibetu'] == !0 ){
-                    $where_str .= "AND seibetu = '". $_GET['seibetu'] . "' ";
+                if(isset($_GET['seibetu']) && !empty($_GET['seibetu'])){
+                    $where_str .= "AND seibetu = '" . $_GET['seibetu'] . "' ";
                 }
 
-                if(isset($_GET['age']) && !empty($_GET['age'])){
-                    $where_str .= "AND age = '". $_GET['age'] . "' ";
-                }
-
-                if($_GET['pref'] == !0 ){
-                    $where_str .= "AND pref = '". $_GET['pref'] . "' ";
-                }
-
-                if($_GET['section_ID'] == !0 ){
+                if(isset($_GET['section_ID']) && !empty($_GET['section_ID'])){
                     $where_str .= "AND section_ID = '". $_GET['section_ID'] . "' ";
                 }
 
-                if($_GET['grade_ID'] == !0 ){
+                if(isset($_GET['grade_ID']) && !empty($_GET['grade_ID'])){
                     $where_str .= "AND grade_ID = '". $_GET['grade_ID'] . "' ";
                 }
 
@@ -97,32 +92,19 @@
                 $result = $sql->fetchAll();
                 $cnt = count($result);
 
+
                 if($cnt == 0){
-                    echo "検索結果：なし";
+                    echo "<tr><td colspan='5'>検索結果なし</td></tr>";
                 }
                 else{
-            ?>
-                    <table border="1" style="border-collapse:collapse" align="center">
-                        <tr>
-                            <th>社員ID</th>
-                            <th>名前</th>
-                            <th>都道府県</th>
-                            <th>性別</th>
-                            <th>年齢</th>
-                            <th>部署</th>
-                            <th>役職</th>
-                    </tr>
-            <?php
                     foreach($result as $member){
                         echo "<tr><td>" . $member['member_ID'] . "</td>";
             ?>
                         <td><a href="./detail01.php?member_ID=<?php echo $member["member_ID"]?>"><?php echo $member["name"]; ?></a></td>
             <?php
-                        echo "<td>" . $pref[$member['pref']] . "</td>";
-                        echo "<td>" . $seibetu[$member['seibetu']] . "</td>";
-                        echo "<td>" . $member['age'] . "</td>";
-                        echo "<td>" . $section_ID[$member['section_ID']] . "</td>";
-                        echo "<td>" . $grade_ID[$member['grade_ID']] . "</td></tr>";
+                        echo "<td>" . $gender_array[$member['seibetu']] . "</td>";
+                        echo "<td>" . $section_ID_array[$member['section_ID']] . "</td>";
+                        echo "<td>" . $grade_ID_array[$member['grade_ID']] . "</td></tr>";
                     }
                     echo "検索結果：" . $cnt . "件";
                 }
