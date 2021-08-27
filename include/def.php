badcdce1744910ace25c3a384a3c2d0445ea7ab8
hamadajunki <hamada-junki@webrage.jp>
2021-08-27 15:24:59 +0900

    test

--- include/def.php
+++ include/def.php
@@ -1,4 +1,10 @@
-<h1>社員名簿システム</h1>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<h1><p class="text-primary">社員名簿システム<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+  <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+</svg></p></h1>
 <p style="text-align:right">
     |
     <a href="./index.php" style="text-align:right" >トップ画面</a>
@@ -7,10 +13,3 @@
     |
 </p>
 <hr>
-
-<?php
-    $DB_DSN = "mysql:host=localhost; dbname=tabe; charset=utf8";
-    $DB_USER = "webaccess";
-    $DB_PW = "toMeu4rH";
-    $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
-?>
